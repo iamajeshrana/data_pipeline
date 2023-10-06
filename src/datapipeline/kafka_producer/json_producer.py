fa596a1fa8460327c9712df0c312448d3acefc04
iamajeshrana <iamajeshrana4u@gmail.com>
2023-10-06 15:42:04 +0530

    Datas Pipeline Perfectly work both producer & Consumer

--- src/datapipeline/kafka_producer/json_producer.py
+++ src/datapipeline/kafka_producer/json_producer.py
@@ -30,7 +30,7 @@ import pandas as pd
 from typing import List
 from src.datapipeline.entity.generic import Generic, instance_to_dict
 
-FILE_PATH = "/home/avnish/iNeuron_Private_Intelligence_Limited/industry_ready_project/projects/data_pipeline/kafka-sensor/sample_data/sensor/aps_failure_training_set1.csv"
+FILE_PATH = "D:\Final Project\Data Science\Modular Programming\ML\Regression\data_pipeline_sensor\sample_data\kafka-sensor-topic\aps_failure_training_set1.csv"
 
 
 def car_to_dict(car: Generic, ctx):
